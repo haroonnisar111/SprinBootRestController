892c1b5ed1ef641119da27b5919315e1fd45cec0
haroonnisar111 <66691075+haroonnisar111@users.noreply.github.com>
2025-05-31 15:14:49 +0530

    exception handling

---
+++ src/main/java/com/SpringRest/demo/entity/Student.java
@@ -0,0 +1,65 @@
+package com.SpringRest.demo.entity;
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Student() {
+        // Default constructor
+    }
+
+    public Student( String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // toString method
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/SpringRest/demo/rest/StudentRestController.java
@@ -0,0 +1,45 @@
+package com.SpringRest.demo.rest;
+
+import com.SpringRest.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+   private List<Student> students;
+
+    @PostConstruct
+    public  void loadData(){
+        students=new ArrayList<>();
+        students.add(new Student("ulfat","auyoob","ulfat@gmail.com"));
+        students.add(new Student("harooon","nisar","haroon@gmail.com"));
+        students.add(new Student("sharaf","rashud","sharaf@gmail.com"));
+
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return  students;
+
+    }
+    @GetMapping("/student/{studentId}")
+    public Student getStudentbyId(@PathVariable int studentId){
+        if(studentId>=students.size()||studentId<0){
+            throw  new StudentNotFoundException("Student id not Found- " + studentId);
+        }
+
+            return  students.get(studentId);
+
+   }
+
+
+
+}
