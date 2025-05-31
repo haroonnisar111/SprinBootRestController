ec5dc22e7eb46b808c886ec385bbcc3b1aeecc4d
haroonnisar111 <66691075+haroonnisar111@users.noreply.github.com>
2025-05-31 16:31:16 +0530

    added global exception handling

--- src/main/java/com/SpringRest/demo/rest/StudentNotFoundException.java
+++ src/main/java/com/SpringRest/demo/rest/StudentNotFoundException.java
@@ -13,4 +13,5 @@ public class StudentNotFoundException extends RuntimeException {
         super(cause);
     }
 
+
 }
--- src/main/java/com/SpringRest/demo/rest/StudentRestController.java
+++ src/main/java/com/SpringRest/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.SpringRest.demo.rest;
 
 import com.SpringRest.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,31 +13,34 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-   private List<Student> students;
+    private List<Student> students;
 
     @PostConstruct
-    public  void loadData(){
-        students=new ArrayList<>();
-        students.add(new Student("ulfat","auyoob","ulfat@gmail.com"));
-        students.add(new Student("harooon","nisar","haroon@gmail.com"));
-        students.add(new Student("sharaf","rashud","sharaf@gmail.com"));
+    public void loadData() {
+        students = new ArrayList<>();
+        students.add(new Student("ulfat", "auyoob", "ulfat@gmail.com"));
+        students.add(new Student("harooon", "nisar", "haroon@gmail.com"));
+        students.add(new Student("sharaf", "rashud", "sharaf@gmail.com"));
 
     }
 
     @GetMapping("/students")
-    public List<Student> getStudents(){
-        return  students;
+    public List<Student> getStudents() {
+        return students;
 
     }
+
     @GetMapping("/student/{studentId}")
-    public Student getStudentbyId(@PathVariable int studentId){
-        if(studentId>=students.size()||studentId<0){
-            throw  new StudentNotFoundException("Student id not Found- " + studentId);
+    public Student getStudentbyId(@PathVariable int studentId) {
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not Found- " + studentId);
         }
 
-            return  students.get(studentId);
+        return students.get(studentId);
 
-   }
+    }
+
+
 
 
 
